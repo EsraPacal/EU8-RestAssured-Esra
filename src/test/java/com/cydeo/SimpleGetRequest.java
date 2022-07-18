ab35e8ee87d49505e2e9d7f206df8b9aedf197c9
esra_pacal <esraksk.61@gmail.com>
2022-07-18 15:51:55 +0200

    first

---
+++ src/test/java/com/cydeo/SimpleGetRequest.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url ="http://54.152.94.133:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        //send a get request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        //print response status code
+        System.out.println(response.getStatusCode());
+
+        //print response body
+        System.out.println(response.prettyPrint());
+
+
+
+
+    }
+}
