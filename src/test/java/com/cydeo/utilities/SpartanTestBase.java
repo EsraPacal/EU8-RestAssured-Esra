38e930dcae274a99c4c4786a2af7d7bbc9e75092
esra_pacal <esraksk.61@gmail.com>
2022-08-07 19:06:48 +0200

    day.5 Hamcrest

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -12,7 +13,21 @@ public class SpartanTestBase {
 
         //save baseurl inside this variable so that we don't need to type each http method.
         baseURI = "http://54.152.94.133:8000";
-
         //get ip address from configurations
+
+        String dbUrl = "jdbc:oracle:thin:@54.152.94.133:1521:xe";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
+
+        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+
+
     }
+
+    @AfterAll
+    public static void teardown(){
+
+        //DBUtils.destroy();
+    }
+
 }
