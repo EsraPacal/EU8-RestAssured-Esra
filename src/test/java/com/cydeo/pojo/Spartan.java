958f57962266d2b2eccacdd813d8ae4324001e9c
esra_pacal <esraksk.61@gmail.com>
2022-08-17 15:18:21 +0200

    Pojo_SpartanClass updated with @Getter_@Setter

--- src/test/java/com/cydeo/pojo/Spartan.java
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -1,6 +1,11 @@
 package com.cydeo.pojo;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /*
 {
     "id": 15,
@@ -10,7 +15,11 @@ package com.cydeo.pojo;
 
  }
  */
+@Getter
+@Setter
+@ToString
 
+@JsonIgnoreProperties(value = "id",allowSetters = true)  //for id, we can only use Setter() method
 public class Spartan {
 
     //getter setter
@@ -20,48 +29,6 @@ public class Spartan {
            private String gender;
            private long phone;
 
-    public int getId() {
-        return id;
-    }
 
-
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }
 
 }
